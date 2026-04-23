275238ec52d91aedfcd9aba5bfba879bf08cb99b
gera <gera@computare.local>
2026-04-23 20:38:56 +0200

    docker compose up

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from art import *
 
 app = FastAPI()
 
 # Allow the React frontend to communicate with this backend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # Vite's default port
+    allow_origins=["http://localhost:3000", "http://localhost:5173"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -16,3 +17,7 @@ app.add_middleware(
 @app.get("/api/message")
 def read_root():
     return {"message": "Hello from FastAPI!"}
+
+@app.get("/api/health")
+def health_check():
+    return {"message": "Pura vida harni tsytsky!", "status": "up", "backend": "running"}
