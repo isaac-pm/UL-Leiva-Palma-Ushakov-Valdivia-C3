b455e3177ea326e4c16123e2ee1d0d401dfdab7d
gera <gera@computare.local>
2026-05-05 20:04:35 +0200

    [Docker from db and coker for app isolated]

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,11 @@
+"""
+Author geralm
+
+Do not touch this file, the based models is the information mapped from the .csv -> SQL table.
+Modifiying this will take us 16:30 hours inserting millions of data 
+if you want (and 'll have) to add custom tables consider  ->>>>  visualization_models.py
+"""
+
 import uuid
 from datetime import datetime, time
 from typing import List, Optional, Dict, Any
